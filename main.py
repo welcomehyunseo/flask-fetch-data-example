4986c45da326f6415c0c789e1160ad283b1c0cf7
welcomehyunseo <92869725+welcomehyunseo@users.noreply.github.com>
2022-04-07 22:37:08 +0900

    save

---
+++ main.py
@@ -0,0 +1,30 @@
+from faker import Faker
+from flask import Flask, jsonify
+from flask.templating import render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/get_users", methods=["GET"])
+def get_users():
+    # logic
+
+    fake = Faker()
+    data = []
+    for i in range(1, 1000):
+        data.append(fake.simple_profile())
+
+    return jsonify({
+        "success": True,
+        "data": data
+    })
+
+
+app.run()
